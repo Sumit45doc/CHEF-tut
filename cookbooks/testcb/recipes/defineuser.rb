d2d653c84294385e091fd010a4c3cad63853b85c
Sumit45doc <ss8559283@gmail.com>
2021-09-07 14:02:20 +0530

    create user and group

---
+++ cookbooks/testcb/recipes/defineuser.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook:: testcb
+# Recipe:: defineuser
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+user "sukriti" do
+action :create
+end
+
+group "family" do
+action :create
+end
